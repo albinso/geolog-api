8c83fb1459ef27ecd06c29858735e62555f8d01a
Albin Söderholm <albin.johso@gmail.com>
2023-06-18 23:49:33 +0200

    Validate fields

--- ClientTrigger/index.js
+++ ClientTrigger/index.js
@@ -3,6 +3,14 @@ module.exports = async function (context, req) {
 
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = 'Got request: ' + JSON.stringify(req.body);
+
+    if (!req.body.latitude || !req.body.longitude) {
+        context.res = {
+            status: 400,
+            body: "Missing required field 'latitude' or 'longitude'"
+        };
+        return;
+    }
     context.res = {
         status: 200, /* Defaults to 200 */
         body: responseMessage
