5ddfd356584e4ffeffb680d0ccbc6cd23c86e783
Albin Söderholm <albin.johso@gmail.com>
2023-06-18 23:34:03 +0200

    Initial function

---
+++ ClientTrigger/index.js
@@ -0,0 +1,10 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = 'Got request: ' + JSON.stringify(req.body);
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
